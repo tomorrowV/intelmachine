562cc292ebaa37d7c48a2188129b938e04f834c6
Vladislav <danilovvlad2@gmail.com>
2023-01-17 17:26:47 +0200

    second header

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,20 +1,20 @@
 const addClass = (elem, className) => elem.classList.add(className);
 const removeClass = (elem, className) => elem.classList.remove(className);
 
-const headerMain = document.querySelector(".main-header");
-if (headerMain) {
+const header = document.querySelector(".header");
+if (header) {
     const top = window.scrollY;
-    if (top > 150) {
-        addClass(headerMain, "scroll");
+    if (top > 50) {
+        addClass(header, "scroll");
     } else {
-        removeClass(headerMain, "scroll");
+        removeClass(header, "scroll");
     }
     window.addEventListener("scroll", () => {
         const top = window.scrollY;
         if (top > 150) {
-            addClass(headerMain, "scroll");
+            addClass(header, "scroll");
         } else {
-            removeClass(headerMain, "scroll");
+            removeClass(header, "scroll");
         }
     });
 }
