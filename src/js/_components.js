b53d0d24094cd749183586275eb2e9d298f87221
Vladislav <danilovvlad2@gmail.com>
2023-01-19 17:06:43 +0200

    modals

--- src/js/_components.js
+++ src/js/_components.js
@@ -31,3 +31,62 @@ if (menu) {
         elements.forEach((el) => toggleClass(el, "active"));
     });
 }
+
+const modalTriggers = document.querySelectorAll("*[data-modal]");
+const modals = document.querySelectorAll(".modal");
+const thanksModals = document.querySelectorAll(".thanks-modal");
+const forms = document.querySelectorAll("form");
+if (modalTriggers && modals) {
+    modalTriggers.forEach((trigger) => {
+        trigger.addEventListener("click", () => {
+            const attr = trigger.dataset.modal;
+            document.getElementById(attr).classList.add("show");
+        });
+    });
+
+    forms.forEach((form) => {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const inputs = form.querySelectorAll("input");
+            const textAreas = form.querySelector("textarea");
+            inputs.forEach((input) => {
+                if (input.getAttribute("type") === "email") {
+                    modals.forEach((t) => removeClass(t, "show"));
+                    inputs.forEach((i) => (i.value = ""));
+                    setTimeout(() => {
+                        addClass(thanksModals[0], "show");
+                    }, 100);
+                    setTimeout(() => {
+                        removeClass(thanksModals[0], "show");
+                    }, 1500);
+                }
+                if (!input.checked) {
+                    return;
+                } else {
+                    modals.forEach((t) => removeClass(t, "show"));
+                    inputs.forEach((i) => (i.value = ""));
+                    if (textAreas) textAreas.value = "";
+                    setTimeout(() => {
+                        addClass(thanksModals[0], "show");
+                    }, 100);
+                    setTimeout(() => {
+                        removeClass(thanksModals[0], "show");
+                    }, 1500);
+                }
+            });
+        });
+    });
+
+    modals.forEach((modal) => {
+        modal.addEventListener("click", (e) => {
+            const target = e.target;
+            const conditions =
+                !target.closest(".modal-form") ||
+                target.closest(".modal-close  > svg");
+
+            if (conditions) {
+                removeClass(modal, "show");
+            }
+        });
+    });
+}
