a792ec0fab9899321a61e116c3121e5d6ead61a2
Vladislav <danilovvlad2@gmail.com>
2023-01-26 20:00:05 +0200

    started animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,169 +5,34 @@ setTimeout(() => {
     document.getElementById("preloader")?.remove();
 }, 500);
 
-const searchBorderFunc = () => {
-    const elements = document.querySelectorAll("[data-border]");
-    elements.forEach(function (element) {
-        let borderVerticalId =
-            typeof element.getAttribute("border-vertical-id") !== "undefined"
-                ? element.getAttribute("border-vertical-id")
-                : !1;
+function searchBorder() {
+    const borders = document.querySelectorAll("[data-border]");
+    borders.forEach((border) => {
+        const borderHorizonAttr = border.getAttribute("border-horizon-id");
+        const borderVericalAttr = border.getAttribute("border-vertical-id");
         let borderHorizonId =
-            typeof element.getAttribute("border-horizon-id") !== "undefined"
-                ? element.getAttribute("border-horizon-id")
-                : !1;
-        if (element.style.display !== "none") {
-            if (borderHorizonId && borderHorizonId !== "#null") {
-                element.setAttribute(
-                    "border-width",
-                    Math.round(element.getBoundingClientRect().width)
-                );
-
-                document.querySelector(
-                    `[border-horizon-id="${borderHorizonId}"]`
-                ).style.cssText = `left: ${
-                    element.style.borderLeftStyle === "solid" ? element : ""
-                }`;
-            }
-        }
-        if (document.querySelector(this).css("display") !== "none") {
-            if (borderVerticalId) {
-                document
-                    .querySelector(this)
-                    .attr(
-                        "border-height",
-                        Math.round(document.querySelector(this).outerHeight())
-                    );
-                document.querySelector(borderVerticalId).css({
-                    left:
-                        document
-                            .querySelector(this)
-                            .css("border-left-style") === "solid"
-                            ? document.querySelector(this).offset().left
-                            : document.querySelector(this).offset().left +
-                              document.querySelector(this).outerWidth() -
-                              1,
-                    top: document.querySelector(this).offset().top,
-                    height: document.querySelector(this).outerHeight(),
-                });
-            }
+            borderHorizonAttr !== null ? borderHorizonAttr : false;
+        let borderVerticalId =
+            borderVericalAttr !== null ? borderVericalAttr : false;
+        if (border.style.display !== "none") {
             if (borderHorizonId) {
-                document
-                    .querySelector(this)
-                    .attr(
-                        "border-width",
-                        Math.round(document.querySelector(this).outerWidth())
-                    );
-                document.querySelector(borderHorizonId).css({
-                    left: document.querySelector(this).offset().left,
-                    top:
-                        document.querySelector(this).css("border-top-style") ===
-                        "solid"
-                            ? document.querySelector(this).offset().top
-                            : document.querySelector(this).offset().top +
-                              document.querySelector(this).outerHeight() -
-                              1,
-                    width: document.querySelector(this).outerWidth(),
-                });
-            }
-            if (
-                (document.querySelector(this).css("border-right-style") ===
-                    "solid" ||
-                    document.querySelector(this).css("border-left-style") ===
-                        "solid") &&
-                !borderVerticalId
-            ) {
-                let borderId = Math.random().toString(16).slice(2);
-                document
-                    .querySelector(this)
-                    .attr(
-                        "border-height",
-                        Math.round(document.querySelector(this).outerHeight())
-                    );
-                document
-                    .querySelector(this)
-                    .attr("border-vertical-id", borderId);
-                document
-                    .querySelector("body .border-store")
-                    .insertAdjacentHTML(
-                        "beforeend",
-                        "<span id='" +
-                            borderId +
-                            "' class='border-element border-vertical " +
-                            document.querySelector(this).attr("border") +
-                            " " +
-                            (this.tagName === "HEADER" ||
-                            document.querySelector(this).closest("header")
-                                .length
-                                ? "overhead"
-                                : "") +
-                            "' " +
-                            "style='left: " +
-                            (document
-                                .querySelector(this)
-                                .css("border-left-style") === "solid"
-                                ? document.querySelector(this).offset().left
-                                : document.querySelector(this).offset().left +
-                                  document.querySelector(this).outerWidth() -
-                                  1) +
-                            "px; " +
-                            "top: " +
-                            document.querySelector(this).offset().top +
-                            "px;" +
-                            "height:" +
-                            document.querySelector(this).outerHeight() +
-                            "px;'></span>"
-                    );
-            }
-            if (
-                (document.querySelector(this).css("border-top-style") ===
-                    "solid" ||
-                    document.querySelector(this).css("border-bottom-style") ===
-                        "solid") &&
-                !borderHorizonId
-            ) {
-                let borderId = Math.random().toString(16).slice(2);
-                document
-                    .querySelector(this)
-                    .attr(
-                        "border-width",
-                        Math.round(document.querySelector(this).outerWidth())
-                    );
-                document
-                    .querySelector(this)
-                    .attr("border-horizon-id", borderId);
-                document
-                    .querySelector("body .border-store")
-                    .insertAdjacentHTML(
-                        "beforeend",
-                        "<span id='" +
-                            borderId +
-                            "' class='border-element border-horizon " +
-                            document.querySelector(this).attr("border") +
-                            " " +
-                            (this.tagName === "HEADER" ||
-                            document.querySelector(this).closest("header")
-                                .length
-                                ? "overhead"
-                                : "") +
-                            "' " +
-                            "style=' left: " +
-                            document.querySelector(this).offset().left +
-                            "px; " +
-                            "top: " +
-                            (document
-                                .querySelector(this)
-                                .css("border-top-style") === "solid"
-                                ? document.querySelector(this).offset().top
-                                : document.querySelector(this).offset().top +
-                                  document.querySelector(this).outerHeight() -
-                                  1) +
-                            "px;" +
-                            "width:" +
-                            document.querySelector(this).outerWidth() +
-                            "px;'></span>"
-                    );
+                const bWidth = Math.round(border.getBoundingClientRect().width);
+                border.setAttribute("border-width", bWidth);
+                const span = document.getElementById(borderHorizonId);
+                span.style.cssText = `
+                    left: ${span.offsetLeft};
+                    top: ${
+                        span.style.borderTopStyle === "solid"
+                            ? span.offsetLeft
+                            : span.offsetTop +
+                              span.getBoundingClientRect().height -
+                              1
+                    };
+                    width: ${span.getBoundingClientRect().width}
+                `;
             }
         }
     });
-};
+}
+
+searchBorder();
